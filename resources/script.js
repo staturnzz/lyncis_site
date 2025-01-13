6b0eafd900d5bac27ce3ef4e68add3b14ade9fcc
staturnz <118011249+staturnzz@users.noreply.github.com>
2025-01-12 21:38:21 -0600

    init

---
+++ resources/script.js
@@ -0,0 +1,3 @@
+/*! Basic iOS 7 CSS // Apache License 2.0 // hbang.ws */
+!function(e){"use strict";var i=e.documentElement,t=i.classList;if(-1!=navigator.userAgent.indexOf("Cydia")?(-1!=e.title.indexOf(" · ")&&(e.title=e.title.split(" · ")[0]),t.add("cydia")):t.remove("cydia","depiction"),"devicePixelRatio"in window&&devicePixelRatio>=2){var d=e.createElement("div");d.style.border=".5px solid transparent",i.appendChild(d),d.offsetHeight>0&&t.add("has-subpixel"),devicePixelRatio>=3&&t.add("has-subpixel-3x"),i.removeChild(d)}}(document);
+//# sourceMappingURL=ios7.min.js.map
